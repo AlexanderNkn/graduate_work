1f48c0b3db3933153557c31981032502792f71cf
Alexander <yspresi@yandex.ru>
2021-12-09 23:58:27 +0300

    added saving state

--- postgres_to_es/etl_process.py
+++ postgres_to_es/etl_process.py
@@ -5,16 +5,17 @@ according batch size, will be fetched from Postgres and immediately upload to El
 Then the process will be repeated untill all data from Postgres would be transfered to Elasticsearch.
 """
 import json
-import math
 from os.path import dirname, join
-from typing import Any, List, Tuple
+from typing import Any, Generator
 
+import elasticsearch
+import psycopg2
 from elasticsearch.helpers import bulk
 
 from utils.connections import ElasticConnection, PostgresConnection
 from utils.etl_state import JsonFileStorage, State
 
-TRANSFER_BATCH_SIZE = 10
+TRANSFER_BATCH_SIZE = 100
 # path for ElasticSearch index schema
 SCHEMA_PATH = join(dirname(__file__), 'es_schema.json')
 # path for ETL latest state
@@ -24,44 +25,53 @@ STATE_PATH = join(dirname(__file__), 'etl_state.json')
 class ETL:
     """Extracts data from Postges then load it to ElasticSearch."""
 
-    def extract(self):
+    def extract(self) -> Generator[list[tuple], None, None]:
         """Retrieve data from Postgres.
         Keeps state of the last call to continue retrieving data starting from
         the save point.
         """
-        storage = JsonFileStorage(STATE_PATH)
-        state = State(storage)
-        latest_update: str = state.get_state(key='latest_update')
-        pg_connection = PostgresConnection()
-        conn = pg_connection.get_connection()
-        with conn.cursor() as cur:
-            sql="""
-                SELECT 
-                    fw.id, fw.rating, fw.title, fw.description,
-                    jsonb_agg(jsonb_build_object('id', p.id, 'full_name', p.full_name, 'role', pfw.role)) AS persons,
-                    jsonb_agg(jsonb_build_object('id', g.id, 'genre', g.name)) AS genres,
-                    GREATEST(fw.updated_at, MAX(p.updated_at), MAX(g.updated_at)) AS latest_update
-                FROM content.film_work fw
-                LEFT OUTER JOIN content.person_film_work pfw ON fw.id = pfw.film_work_id
-                LEFT OUTER JOIN content.person p ON p.id = pfw.person_id
-                LEFT OUTER JOIN content.genre_film_work gfw ON fw.id = gfw.film_work_id
-                LEFT OUTER JOIN content.genre g ON gfw.genre_id = g.id
-                WHERE fw.updated_at > %s or p.updated_at > %s or g.updated_at > %s
-                GROUP BY fw.id
-                ORDER BY latest_update desc;
-            """
-            cur.execute(sql, (latest_update, latest_update, latest_update))
-            while True:
-                batch = cur.fetchmany(TRANSFER_BATCH_SIZE)
-                if not batch:
+        while True:
+            storage = JsonFileStorage(STATE_PATH)
+            state = State(storage)
+            latest_update: str = state.get_state(key='latest_update')
+            pg_connection = PostgresConnection()
+            conn = pg_connection.get_connection()
+            with conn.cursor() as cur:
+                sql = """
+                    SELECT
+                        fw.id, fw.rating, fw.title, fw.description,
+                        jsonb_agg(jsonb_build_object('id', p.id, 'full_name', p.full_name, 'role', pfw.role)) AS persons,
+                        jsonb_agg(jsonb_build_object('id', g.id, 'genre', g.name)) AS genres,
+                        GREATEST(fw.updated_at, MAX(p.updated_at), MAX(g.updated_at)) AS latest_update
+                    FROM content.film_work fw
+                    LEFT OUTER JOIN content.person_film_work pfw ON fw.id = pfw.film_work_id
+                    LEFT OUTER JOIN content.person p ON p.id = pfw.person_id
+                    LEFT OUTER JOIN content.genre_film_work gfw ON fw.id = gfw.film_work_id
+                    LEFT OUTER JOIN content.genre g ON gfw.genre_id = g.id
+                    WHERE fw.updated_at > %s or p.updated_at > %s or g.updated_at > %s
+                    GROUP BY fw.id
+                    ORDER BY latest_update;
+                """
+                try:
+                    cur.execute(sql, (latest_update, latest_update, latest_update))
+                except (psycopg2.OperationalError, psycopg2.errors.AdminShutdown):
+                    continue
+                except Exception:
                     break
-                yield batch
-            
+                else:
+                    while True:
+                        batch: list[tuple] = cur.fetchmany(TRANSFER_BATCH_SIZE)
+                        if not batch:
+                            break
+                        yield batch
+                        latest_update = batch[-1][-1].strftime('%Y-%m-%d %H:%M:%S.%f')
+                        state.set_state(key='latest_update', value=latest_update)
+                    break
 
-    def transform(self, data: List[Tuple]):
+    def transform(self, data: Generator[list[tuple], None, None]) -> Generator[dict[str, Any], None, None]:
         """Transforms raw data to required by ElasticSearch format."""
         for row in data:
-            for id, rating, title, description, persons, genres, latest_update in row:
+            for id, rating, title, description, persons, genres, _ in [*row]:
                 genre = ' '.join({item['genre'] for item in genres if item.get('genre')})
                 actors_names, actors, writers_names, writers, directors_names = [], [], [], [], []
                 unique_persons = list({person['id']: person for person in persons}.values())
@@ -93,27 +103,31 @@ class ETL:
                 }
                 yield doc
 
-
-    def load(self, data):
+    def load(self, data: Generator[dict[str, Any], None, None]) -> None:
         """Load data to Elasticsearch.
         Keeps state of the last call to continue loading data starting from
         the save point.
         """
-        es_connection = ElasticConnection()
-        client = es_connection.get_client()
-        self.create_index(client)
-        try:
-            bulk(
-                client=client,
-                index='movies',
-                actions=data,
-                chunk_size=TRANSFER_BATCH_SIZE,
-                max_retries=1000,
-                initial_backoff=1,
-                max_backoff=300,
-            )
-        except Exception:
-            raise
+        while True:
+            es_connection = ElasticConnection()
+            client = es_connection.get_client()
+            try:
+                self.create_index(client)
+                bulk(
+                    client=client,
+                    index='movies',
+                    actions=data,
+                    chunk_size=TRANSFER_BATCH_SIZE,
+                    max_retries=1000,
+                    initial_backoff=1,
+                    max_backoff=300,
+                )
+            except elasticsearch.ConnectionError:
+                continue
+            except Exception:
+                break
+            else:
+                break
 
     def create_index(self, client):
         """Creates an index in Elasticsearch if one isn't already there."""
@@ -124,11 +138,12 @@ class ETL:
                 ignore=400,
             )
 
-    def run(self):
+    def run(self) -> None:
         """Runs ETL processes."""
         raw_data = self.extract()
         prepared_data = self.transform(raw_data)
         self.load(prepared_data)
+
 
 if __name__ == '__main__':
     etl_process = ETL()
--- postgres_to_es/utils/connections.py
+++ postgres_to_es/utils/connections.py
@@ -3,9 +3,9 @@ from os.path import dirname, join
 from typing import Optional
 
 import psycopg2
-from psycopg2.extensions import connection as PGconnection
 from dotenv import load_dotenv
 from elasticsearch import Elasticsearch
+from psycopg2.extensions import connection as PGconnection
 
 from .backoff import backoff
 
@@ -14,21 +14,18 @@ load_dotenv(dotenv_path)
 
 
 class ElasticConnection:
+    """Provides method for automatic reconnection to Elastic db."""
     def __init__(self) -> None:
         self.socket = {'host': os.getenv('ES_HOST'), 'port': os.getenv('ES_PORT')}
         self._client = Elasticsearch([self.socket])
 
     @backoff(initial_backoff=1, max_backoff=10)
     def get_client(self) -> Optional[Elasticsearch]:
-        # return self._client if self._client.ping() else None
-        if self._client.ping():
-            return self._client
-        else:
-            print('Elastic failed')
-            return None
+        return self._client if self._client.ping() else None
 
 
 class PostgresConnection:
+    """Provides method for automatic reconnection to Postgres db."""
     def __init__(self) -> None:
         self.dsn = {
             'dbname': os.getenv('PG_DBNAME'),
@@ -44,5 +41,4 @@ class PostgresConnection:
         try:
             return psycopg2.connect(**self.dsn)
         except psycopg2.OperationalError:
-            print('Postgres failed')
             return None
