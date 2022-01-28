bcb5b5695cda2f8612cc0fa1e3250d39f50429cd
AlexanderNkn <yspresi@yandex.ru>
2022-01-28 22:59:32 +0300

    improved context manager

--- tests/functional/src/test_genre.py
+++ tests/functional/src/test_genre.py
@@ -1,25 +1,31 @@
-import pytest
 from http import HTTPStatus
+
+import pytest
+import pytest_asyncio
 
 from ..testdata.genre import genre_by_id_expected, genre_list, genre_list_expected
 
 pytestmark = pytest.mark.asyncio
 
 
-async def test_get_genre_by_id(send_data_to_elastic, genre_list, make_get_request, genre_by_id_expected):
+@pytest_asyncio.fixture
+async def upload_genre_data(send_data_to_elastic, genre_list):
     async with send_data_to_elastic(data=genre_list):
-        response = await make_get_request('/genre/32345678-1234-1234-1234-123456789101')
-
-        assert response.status == HTTPStatus.OK, 'genre doesn\'t available by id'
-        assert len(response.body) == len(genre_by_id_expected), 'check fields count'
-        assert response.body == genre_by_id_expected, 'check data in document'
+        yield
 
 
-async def test_get_nonexistent_genre(send_data_to_elastic, genre_list, make_get_request):
-    async with send_data_to_elastic(data=genre_list):
-        response = await make_get_request('/genre/32345678-1234-1234-1234-123456789100')
+async def test_get_genre_by_id(upload_genre_data, make_get_request, genre_by_id_expected):
+    response = await make_get_request('/genre/32345678-1234-1234-1234-123456789101')
 
-        assert response.status == HTTPStatus.NOT_FOUND, 'available nonexistent genre'
+    assert response.status == HTTPStatus.OK, 'genre doesn\'t available by id'
+    assert len(response.body) == len(genre_by_id_expected), 'check fields count'
+    assert response.body == genre_by_id_expected, 'check data in document'
+
+
+async def test_get_nonexistent_genre(upload_genre_data, make_get_request):
+    response = await make_get_request('/genre/32345678-1234-1234-1234-123456789100')
+
+    assert response.status == HTTPStatus.NOT_FOUND, 'available nonexistent genre'
 
 
 async def test_get_cached_genre(
@@ -45,64 +51,55 @@ async def test_get_cached_genre(
     assert response.status == HTTPStatus.NOT_FOUND, 'data in cache still exists after deletion'
 
 
-async def test_full_genre_list(send_data_to_elastic, genre_list, make_get_request, genre_list_expected):
-    async with send_data_to_elastic(data=genre_list):
-        response = await make_get_request('/genre')
+async def test_full_genre_list(upload_genre_data, make_get_request, genre_list_expected):
+    response = await make_get_request('/genre')
 
-        assert response.status == HTTPStatus.OK, 'genre list should be available'
-        assert len(response.body) == len(genre_list_expected), 'check genre count'
-        key_sort = lambda genre_info: genre_info['uuid']
-        assert sorted(response.body, key=key_sort) == sorted(genre_list_expected, key=key_sort), \
-            'check data in documents'
+    assert response.status == HTTPStatus.OK, 'genre list should be available'
+    assert len(response.body) == len(genre_list_expected), 'check genre count'
+    key_sort = lambda genre_info: genre_info['uuid']
+    assert sorted(response.body, key=key_sort) == sorted(genre_list_expected, key=key_sort), \
+        'check data in documents'
 
 
-async def test_pagination_first_page_size(send_data_to_elastic, genre_list, make_get_request, genre_list_expected):
+async def test_pagination_first_page_size(upload_genre_data, make_get_request, genre_list_expected):
     # testing scenario:
     # we select page size that there are n-1 record on first page and 1 record on second page
+    page_size = min(len(genre_list_expected) - 1, 29)
+    response = await make_get_request(f'/genre?page[size]={page_size}&page[number]=1')
 
-    async with send_data_to_elastic(data=genre_list):
-        page_size = min(len(genre_list_expected) - 1, 29)
-        response = await make_get_request(f'/genre?page[size]={page_size}&page[number]=1')
-
-        assert response.status == HTTPStatus.OK, 'pagination should be available'
-        assert len(response.body) == page_size, 'check genre count'
+    assert response.status == HTTPStatus.OK, 'pagination should be available'
+    assert len(response.body) == page_size, 'check genre count'
 
 
-async def test_pagination_second_page_size(send_data_to_elastic, genre_list, make_get_request, genre_list_expected):
+async def test_pagination_second_page_size(upload_genre_data, make_get_request, genre_list_expected):
     # testing scenario:
     # we select page size that there are n-1 record on first page and 1 record on second page
+    page_size = len(genre_list_expected) - 1
+    response = await make_get_request(f'/genre?page[size]={page_size}&page[number]=2')
 
-    async with send_data_to_elastic(data=genre_list):
-        page_size = len(genre_list_expected) - 1
-        response = await make_get_request(f'/genre?page[size]={page_size}&page[number]=2')
-
-        assert response.status == HTTPStatus.OK, 'pagination should be available'
-        assert len(response.body) == 1, 'check genre count'
+    assert response.status == HTTPStatus.OK, 'pagination should be available'
+    assert len(response.body) == 1, 'check genre count'
 
 
-async def test_pagination_page_size_negative(send_data_to_elastic, genre_list, make_get_request):
-    async with send_data_to_elastic(data=genre_list):
-        response = await make_get_request('/genre?page[size]=-1')
-        assert response.status == HTTPStatus.BAD_REQUEST
+async def test_pagination_page_size_negative(upload_genre_data, make_get_request):
+    response = await make_get_request('/genre?page[size]=-1')
+    assert response.status == HTTPStatus.BAD_REQUEST
 
 
-async def test_pagination_page_size_not_number(send_data_to_elastic, genre_list, make_get_request):
-    async with send_data_to_elastic(data=genre_list):
-        response = await make_get_request('/genre?page[size]=a')
-        assert response.status == HTTPStatus.BAD_REQUEST
+async def test_pagination_page_size_not_number(upload_genre_data, make_get_request):
+    response = await make_get_request('/genre?page[size]=a')
+    assert response.status == HTTPStatus.BAD_REQUEST
 
 
-async def test_genre_text_search_by_title(send_data_to_elastic, genre_list, make_get_request):
-    async with send_data_to_elastic(data=genre_list):
-        response = await make_get_request('/genre/search?query=show')
+async def test_genre_text_search_by_title(upload_genre_data, make_get_request):
+    response = await make_get_request('/genre/search?query=show')
 
-        assert response.status == HTTPStatus.OK, 'text search should be available'
-        assert len(response.body) == 2, 'search by name doesn\'t available'
+    assert response.status == HTTPStatus.OK, 'text search should be available'
+    assert len(response.body) == 2, 'search by name doesn\'t available'
 
 
-async def test_genre_text_search_by_description(send_data_to_elastic, genre_list, make_get_request):
-    async with send_data_to_elastic(data=genre_list):
-        response = await make_get_request('/genre/search?query=science')
+async def test_genre_text_search_by_description(upload_genre_data, make_get_request):
+    response = await make_get_request('/genre/search?query=science')
 
-        assert response.status == HTTPStatus.OK, 'text search should be available'
-        assert len(response.body) == 2, 'search by description doesn\'t available'
+    assert response.status == HTTPStatus.OK, 'text search should be available'
+    assert len(response.body) == 2, 'search by description doesn\'t available'
--- tests/functional/src/test_person.py
+++ tests/functional/src/test_person.py
@@ -1,5 +1,7 @@
-import pytest
 from http import HTTPStatus
+
+import pytest
+import pytest_asyncio
 
 from ..testdata.film import film_list
 from ..testdata.person import film_list_expected, person_by_id_expected, person_list, person_list_expected
@@ -7,20 +9,23 @@ from ..testdata.person import film_list_expected, person_by_id_expected, person_
 pytestmark = pytest.mark.asyncio
 
 
-async def test_get_person_by_id(send_data_to_elastic, person_list, make_get_request, person_by_id_expected):
+@pytest_asyncio.fixture
+async def upload_person_data(send_data_to_elastic, person_list):
     async with send_data_to_elastic(data=person_list):
-        response = await make_get_request('/person/22345678-1234-1234-1234-123456789101')
-
-        assert response.status == HTTPStatus.OK, 'person doesn\'t available by id'
-        assert len(response.body) == len(person_by_id_expected), 'check fields count'
-        assert response.body == person_by_id_expected, 'check data in document'
+        yield
 
 
-async def test_get_nonexistent_person(send_data_to_elastic, person_list, make_get_request):
-    async with send_data_to_elastic(data=person_list):
-        response = await make_get_request('/person/22345678-1234-1234-1234-123456789100')
+async def test_get_person_by_id(upload_person_data, make_get_request, person_by_id_expected):
+    response = await make_get_request('/person/22345678-1234-1234-1234-123456789101')
 
-        assert response.status == HTTPStatus.NOT_FOUND, 'available nonexistent person'
+    assert response.status == HTTPStatus.OK, 'person doesn\'t available by id'
+    assert len(response.body) == len(person_by_id_expected), 'check fields count'
+    assert response.body == person_by_id_expected, 'check data in document'
+
+
+async def test_get_nonexistent_person(upload_person_data, make_get_request):
+    response = await make_get_request('/person/22345678-1234-1234-1234-123456789100')
+    assert response.status == HTTPStatus.NOT_FOUND, 'available nonexistent person'
 
 
 async def test_get_cached_person(
@@ -46,76 +51,68 @@ async def test_get_cached_person(
     assert response.status == HTTPStatus.NOT_FOUND, 'data in cache still exists after deletion'
 
 
-async def test_full_person_list(send_data_to_elastic, person_list, make_get_request, person_list_expected):
-    async with send_data_to_elastic(data=person_list):
-        response = await make_get_request('/person')
+async def test_full_person_list(upload_person_data, make_get_request, person_list_expected):
+    response = await make_get_request('/person')
 
-        assert response.status == HTTPStatus.OK, 'person list should be available'
-        assert len(response.body) == len(person_list_expected), 'check person count'
+    assert response.status == HTTPStatus.OK, 'person list should be available'
+    assert len(response.body) == len(person_list_expected), 'check person count'
+    key_sort = lambda person_info: person_info['uuid']
+    assert sorted(response.body, key=key_sort) == sorted(person_list_expected, key=key_sort), \
+        'check data in documents'
+
+
+async def test_person_film_list(
+    send_data_to_elastic, upload_person_data, film_list, make_get_request, film_list_expected
+):
+    async with send_data_to_elastic(data=film_list):
+        response = await make_get_request('/person/22345678-1234-1234-1234-123456789102/film')
+
+        assert response.status == HTTPStatus.OK, 'film list by person should be available'
+        assert len(response.body) == len(film_list_expected), 'check film count'
         key_sort = lambda person_info: person_info['uuid']
-        assert sorted(response.body, key=key_sort) == sorted(person_list_expected, key=key_sort), \
+        assert sorted(response.body, key=key_sort) == sorted(film_list_expected, key=key_sort), \
             'check data in documents'
 
 
-async def test_person_film_list(send_data_to_elastic, person_list, film_list, make_get_request, film_list_expected):
-    async with send_data_to_elastic(data=person_list):
-        async with send_data_to_elastic(data=film_list):
-            response = await make_get_request('/person/22345678-1234-1234-1234-123456789102/film')
-
-            assert response.status == HTTPStatus.OK, 'film list by person should be available'
-            assert len(response.body) == len(film_list_expected), 'check film count'
-            key_sort = lambda person_info: person_info['uuid']
-            assert sorted(response.body, key=key_sort) == sorted(film_list_expected, key=key_sort), \
-                'check data in documents'
-
-
-async def test_pagination_first_page_size(send_data_to_elastic, person_list, make_get_request, person_list_expected):
+async def test_pagination_first_page_size(upload_person_data, make_get_request, person_list_expected):
     # testing scenario:
     # we select page size that there are n-1 record on first page and 1 record on second page
+    page_size = min(len(person_list_expected) - 1, 29)
+    response = await make_get_request(f'/person?page[size]={page_size}&page[number]=1')
 
-    async with send_data_to_elastic(data=person_list):
-        page_size = min(len(person_list_expected) - 1, 29)
-        response = await make_get_request(f'/person?page[size]={page_size}&page[number]=1')
-
-        assert response.status == HTTPStatus.OK, 'pagination should be available'
-        assert len(response.body) == page_size, 'check person count'
+    assert response.status == HTTPStatus.OK, 'pagination should be available'
+    assert len(response.body) == page_size, 'check person count'
 
 
-async def test_pagination_second_page_size(send_data_to_elastic, person_list, make_get_request, person_list_expected):
+async def test_pagination_second_page_size(upload_person_data, make_get_request, person_list_expected):
     # testing scenario:
     # we select page size that there are n-1 record on first page and 1 record on second page
+    page_size = len(person_list_expected) - 1
+    response = await make_get_request(f'/person?page[size]={page_size}&page[number]=2')
 
-    async with send_data_to_elastic(data=person_list):
-        page_size = len(person_list_expected) - 1
-        response = await make_get_request(f'/person?page[size]={page_size}&page[number]=2')
-
-        assert response.status == HTTPStatus.OK, 'pagination should be available'
-        assert len(response.body) == 1, 'check person count'
+    assert response.status == HTTPStatus.OK, 'pagination should be available'
+    assert len(response.body) == 1, 'check person count'
 
 
-async def test_pagination_page_size_negative(send_data_to_elastic, person_list, make_get_request):
-    async with send_data_to_elastic(data=person_list):
-        response = await make_get_request('/person?page[size]=-1')
-        assert response.status == HTTPStatus.BAD_REQUEST
+async def test_pagination_page_size_negative(upload_person_data, make_get_request):
+    response = await make_get_request('/person?page[size]=-1')
+    assert response.status == HTTPStatus.BAD_REQUEST
 
 
-async def test_pagination_page_size_not_number(send_data_to_elastic, person_list, make_get_request):
-    async with send_data_to_elastic(data=person_list):
-        response = await make_get_request('/person?page[size]=a')
-        assert response.status == HTTPStatus.BAD_REQUEST
+async def test_pagination_page_size_not_number(upload_person_data, make_get_request):
+    response = await make_get_request('/person?page[size]=a')
+    assert response.status == HTTPStatus.BAD_REQUEST
 
 
-async def test_person_text_search_by_name(send_data_to_elastic, person_list, make_get_request):
-    async with send_data_to_elastic(data=person_list):
-        response = await make_get_request('/person/search?query=chris')
+async def test_person_text_search_by_name(upload_person_data, make_get_request):
+    response = await make_get_request('/person/search?query=chris')
 
-        assert response.status == HTTPStatus.OK, 'text search should be available'
-        assert len(response.body) == 2, 'search by name doesn\'t available'
+    assert response.status == HTTPStatus.OK, 'text search should be available'
+    assert len(response.body) == 2, 'search by name doesn\'t available'
 
 
-async def test_person_text_search_by_role(send_data_to_elastic, person_list, make_get_request):
-    async with send_data_to_elastic(data=person_list):
-        response = await make_get_request('/person/search?query=actor')
+async def test_person_text_search_by_role(upload_person_data, make_get_request):
+    response = await make_get_request('/person/search?query=actor')
 
-        assert response.status == HTTPStatus.OK, 'text search should be available'
-        assert len(response.body) == 4, 'search by role doesn\'t available'
+    assert response.status == HTTPStatus.OK, 'text search should be available'
+    assert len(response.body) == 4, 'search by role doesn\'t available'
