c765b51b586ba20d73c1a213bb632babf48986f1
AlexanderNkn <yspresi@yandex.ru>
2022-02-04 22:07:26 +0300

    lil fixes

--- tests/settings.py
+++ tests/settings.py
@@ -6,18 +6,18 @@ REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = int(os.getenv('REDIS_PORT', 6399))
 
 POSTGRE_HOST = os.getenv('POSTGRE_HOST', 'localhost')
-POSTGRE_PORT = int(os.getenv('POSTGRE_PORT', 5432))
+POSTGRE_PORT = int(os.getenv('POSTGRE_PORT', 5433))
 POSTGRE_NAME = os.getenv('POSTGRE_NAME', 'movies_database')
 POSTGRE_USER = os.getenv('POSTGRE_USER', 'postgre')
 POSTGRE_PASSWORD = os.getenv('POSTGRE_USER', 'postgre')
 POSTGRE_OPTIONS = os.getenv('POSTGRE_OPTIONS', '-c search_path=users')
 
 FLASK_HOST = os.getenv('FLASK_HOST', '127.0.0.1')
-FLASK_PORT = int(os.getenv('FLASK_PORT', 50))
-BASE_URL = os.getenv('BASE_URL', '/auth/v1') #как вариант разделить корневой эндпоинт
+FLASK_PORT = int(os.getenv('FLASK_PORT', 5000))
+BASE_URL = os.getenv('BASE_URL', '/api/v1')
 
 
-class PostgreTestSettings(BaseSettings):
+class PostgreSettings(BaseSettings):
     host: str = Field(POSTGRE_HOST)
     port: int = Field(POSTGRE_PORT)
     dbname: str = Field(POSTGRE_NAME)
@@ -27,7 +27,7 @@ class PostgreTestSettings(BaseSettings):
 
 
 class TestSettings(BaseSettings):
-    dsn: PostgreTestSettings
+    dsn: PostgreSettings
     redis_url: str = Field(f'redis://{REDIS_HOST}:{REDIS_PORT}')
     service_url: str = Field(f'{FLASK_HOST}:{FLASK_PORT}{BASE_URL}', description='url for auth service')
 
