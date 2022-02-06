c2812a01938d11776b30fa4592ecc01af5f2a49d
AlexanderNkn <yspresi@yandex.ru>
2022-02-06 18:18:06 +0300

    added documentation for roles

--- auth/api/v1/role.py
+++ auth/api/v1/role.py
@@ -1,11 +1,9 @@
 from flask import Blueprint
-from flasgger import swag_from
 
 blueprint = Blueprint('role', __name__, url_prefix='/api/v1')
 
 
-@swag_from('/docs/role_list.yml')
-@blueprint.route('/role', methods=('GET',))
+@blueprint.route('/role')
 async def get_role_list():
     pass
 
@@ -15,26 +13,26 @@ async def create_role():
     pass
 
 
-@blueprint.route('/role/<uuid:id>', methods=('GET',))
+@blueprint.route('/role/<uuid:role_id>')
 async def get_role_by_id():
     pass
 
 
-@blueprint.route('/role/<uuid:id>', methods=('PUT',))
+@blueprint.route('/role/<uuid:role_id>', methods=('PATCH',))
 async def change_role():
     pass
 
 
-@blueprint.route('/role/<uuid:id>', methods=('DELETE',))
+@blueprint.route('/role/<uuid:role_id>', methods=('DELETE',))
 async def delete_role():
     pass
 
 
-@blueprint.route('/assign_role', methods=('POST',))
-async def assign_role():
+@blueprint.route('/assign_roles', methods=('POST',))
+async def assign_roles():
     pass
 
 
-@blueprint.route('/check_permissions', methods=('GET',))
+@blueprint.route('/check_permissions', methods=('POST',))
 async def check_permissions():
     pass
