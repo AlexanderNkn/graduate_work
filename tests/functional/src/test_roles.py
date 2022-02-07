0efcfa3a5e6f2ffe866a027485fdca4beefac4c3
Vladislav Bronzov <vlados@Vladislavs-MBP.Dlink>
2022-02-07 11:17:45 +0300

    some changes and create superuser comm

--- tests/functional/src/test_roles.py
+++ tests/functional/src/test_roles.py
@@ -4,7 +4,7 @@ from http import HTTPStatus
 def test_get_role_list(test_client, roles_list):
     response = test_client.get('/role')
     assert response.status_code == HTTPStatus.OK
-    assert roles_list in response.data
+    assert response.json == roles_list
 
 def test_create_role(test_client, admin_access_token):
     response = test_client.post('/role', json={
@@ -21,7 +21,7 @@ def test_create_role_without_admin_permission(test_client, user_access_token):
 def test_get_role_by_id(test_client, role_by_id_expected):
     response = test_client.get('/role/a9c6e8da-f2bf-458a-978b-d2f50a031451')
     assert response.status_code == HTTPStatus.OK
-    assert role_by_id_expected == response.data
+    assert response.json == role_by_id_expected
 
 def test_get_non_existing_role_by_id(test_client):
     response = test_client.get('/role/444433332222')
@@ -31,7 +31,7 @@ def test_chage_role_details(test_client, role_by_id_expected):
     response = test_client.patch('/role/a9c6e8da-f2bf-458a-978b-d2f50a031451', json={
         'code': 'admin', 'description': 'unlimited access to all actions'})
     assert response.status_code == HTTPStatus.OK
-    assert role_by_id_expected == response.data
+    assert response.json == role_by_id_expected
 
 def test_delete_role(test_client, admin_access_token):
     response = test_client.delete('/role/7cf56926-054c-4522-ac6f-d9f5d0e9d18e', headers=admin_access_token)
@@ -47,7 +47,7 @@ def test_assign_roles(test_client, assigned_roles_to_user, admin_access_token):
         'role_ids': ['a9c6e8da-f2bf-458a-978b-d2f50a031451', '7cf56926-054c-4522-ac6f-d9f5d0e9d18e'],
         }, headers=admin_access_token)
     assert response.status_code == HTTPStatus.CREATED
-    assert assigned_roles_to_user == response.data
+    assert response.json == assigned_roles_to_user
 
 def test_assign_roles_without_admin_permissions(test_client, user_access_token):
     response = test_client.post('/assign_roles', json={
@@ -61,7 +61,7 @@ def test_check_permissions(test_client, admin_access_token):
         "user_id": "cd483e9-5888-40fd-813a-a382154bcfd2", 
         "role_ids": ["a9c6e8da-f2bf-458a-978b-d2f50a031451", "7cf56926-054c-4522-ac6f-d9f5d0e9d18e"]}, headers=admin_access_token)
     assert response.status_code == HTTPStatus.OK
-    assert response.data == True
+    assert response.json == {'has_permissions': True}
 
 def test_check_permissions_without_admin_permission(test_client, user_access_token):
     response = test_client.post('/check_permissions', json={
