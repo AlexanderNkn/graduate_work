74c6c03ad63172578d4e470aedf2504a34bb9cb5
Vladislav Bronzov <vlados@Vladislavs-MBP.Dlink>
2022-02-08 16:17:27 +0300

    added users fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,8 +9,32 @@ from .settings import settings
 
 
 @pytest.fixture
+def app():
+    app = Flask(__name__)
+    app.register_blueprint(auth.blueprint)
+    app.register_blueprint(role.blueprint)
+
+    return app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def create_admin_user(db):
+    user = User(username='test_admin', pwd_hash=generate_password_hash('admin'), is_superuser=True)
+    db.session.add(user)
+    db.session.commit()
+
+@pytest.fixture
+def create_user(db):
+    user = User(username='test_user', pwd_hash=generate_password_hash('user'))
+    db.session.add(user)
+    db.session.commit()
+
+@pytest.fixture
 def admin_token(client):
-    response = client.post('/login', json={'username': 'username', 'password': 'password'}
+    response = client.post('/login', json={'username': 'test_admin', 'password': 'admin'}
     )
     access_token = response.tokens.access_token
     headers = {
@@ -20,7 +44,7 @@ def admin_token(client):
 
 @pytest.fixture
 def user_token(client):
-    response = client.post('/login', json={'username': 'username', 'password': 'password'}
+    response = client.post('/login', json={'username': 'test_user', 'password': 'user'}
     )
     access_token = response.tokens.access_token
     headers = {
