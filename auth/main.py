2fbb47855c3ad9993b9870bf56756e4bf2896bb4
Vladislav Bronzov <vlados@Vladislavs-MBP.Dlink>
2022-02-08 21:26:28 +0300

    some changes for create superuser comm

--- auth/main.py
+++ auth/main.py
@@ -23,6 +23,10 @@ asgi_app = WsgiToAsgi(app)
 def create_superuser(name, password):
     user = User(username=name, pwd_hash=generate_password_hash(password), is_superuser=True)
     db.session.add(user)
+
+    admin_role = Role.query.filter_by(code="admin").first()
+    user_role = UserRole(user_id=user.id, role_id=admin_role.id)
+    db.session.add(user_role)
     db.session.commit()
 
 
