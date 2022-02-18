84ad7ff9948a44ae0d49b0fb2eb06fca9055e6da
aximon <aximon@yandex.ru>
2022-02-18 18:42:59 +0300

    lil fix

--- auth/init_data/roles.py
+++ auth/init_data/roles.py
@@ -6,11 +6,6 @@ ROLES = [
         "permissions": ["users", "personal_data", "roles"]
     },
     {
-        "code": "admin2",
-        "description": "unlimited access to all actions",
-        "permissions": ["users", "personal_data", "roles", "test"]
-    },
-    {
         "code": "subscriber",
         "description": "account without paying for registered users",
     },
