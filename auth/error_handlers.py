ff4cdcf6acff5578388c3687492227b97e319478
AlexanderNkn <yspresi@yandex.ru>
2022-02-18 12:16:50 +0300

    added auto rollback when 500 error

--- auth/error_handlers.py
+++ auth/error_handlers.py
@@ -2,12 +2,19 @@ from typing import Callable
 from flask import json, Flask
 from werkzeug.exceptions import InternalServerError
 
+from extensions import db
+
+
+def rollback_transaction():
+    db.session.rollback()
+
 
 def register_500_error(app: Flask, sentry_event: Callable):
     @app.errorhandler(InternalServerError)
-    def handle_exception(e):
-        """Return JSON with sentry task id."""
-        response = e.get_response()
+    def handle_exception(error):
+        """Rollback session with exception then return JSON with sentry task id."""
+        rollback_transaction()
+        response = error.get_response()
         response.data = json.dumps({
             'status': 'success',
             'message': 'Something went wrong with server',
