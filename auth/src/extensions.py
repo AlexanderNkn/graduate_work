fe194d1d365e217931c0c58890f149ffa3ba8e31
AlexanderNkn <yspresi@yandex.ru>
2022-02-22 23:47:46 +0300

    added alembic

--- auth/src/extensions.py
+++ auth/src/extensions.py
@@ -3,12 +3,14 @@ from flask_caching import Cache
 from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+from flask_alembic import Alembic
 
 jwt = JWTManager()
 db = SQLAlchemy()
 ma = Marshmallow()
 oauth = OAuth()
 cache = Cache()
+alembic = Alembic()
 
 # cache alias for flask-jwt-extended
 jwt_redis_blocklist = cache
